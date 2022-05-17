465b8d34b5ba0e3e6ed078a7247111e1aa4b3197
dang.le <dang06072000@gmail.com>
2022-05-17 16:38:33 +0700

    add list, pagination, applied handlerbar

--- action.js
+++ action.js
@@ -1,12 +1,15 @@
 let sliderIndex = 1;
-let dotIndex = 1;
 let dataList = [];
 let userList = [];
+let bigData = [];
+let currentIndexPage = 1;
+let rowPerPage = 5;
 
 async function FetchData(){
     const response = await  fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
     const data = await response.json();
     dataList = data.slice(0, 10);
+    bigData = data;
     console.log(dataList);
     const responseUser = await fetch('https://jsonplaceholder.typicode.com/users')
     const dataUser = await responseUser.json();
@@ -98,11 +101,105 @@ function CreateThumnails(num){
     }
 }
 
+function ShowCurrentPage(index){
+    currentIndexPage = parseInt(index);
+    RefreshPagi();
+}
+
+function HandlerNext (){
+    let sumPage = Math.ceil(bigData.length/ rowPerPage);
+    currentIndexPage ++;
+    if(currentIndexPage > sumPage){
+        currentIndexPage = 1
+    }
+    RefreshPagi()
+}
+
+function HandlerPrev (){
+    let sumPage = Math.ceil(bigData.length/ rowPerPage);
+    currentIndexPage --;
+    if(currentIndexPage < 1){
+        currentIndexPage = sumPage
+    }
+    RefreshPagi()
+}
+
+//reset active pagination
+function RefreshPagi(){
+    HandlerBarFuc();
+    let numberPagi = document.getElementsByClassName("pagi-item");
+    for (let i = 0; i < numberPagi.length; i++) {
+        numberPagi[i].className = numberPagi[i].className.replace(" active-pagi-item", "");
+    }
+    for (let x = 0; x < numberPagi.length; x++) {
+        if(x == (currentIndexPage - 1)){
+            numberPagi[x].className += (" active-pagi-item");
+        }
+    }
+}
+
+function ChangeRowsPerPage(){
+    let selectedItem = document.getElementById('rowsPerPage');
+    rowPerPage = selectedItem.value;
+    HandlerBarFuc();
+    CreatePagination(); 
+}
+
+function CreatePagination (){
+    //pagination
+    let sourcePagi = document.getElementById('container-pagination');
+    sourcePagi.innerHTML = '';
+    let prevBtn = document.createElement('div');
+    prevBtn.className = "pagi-item-prev";
+    prevBtn.innerHTML = "Prev";
+    prevBtn.addEventListener('click', function(){
+        HandlerPrev();
+    })
+    sourcePagi.appendChild(prevBtn);
+    let sumPage = Math.ceil(bigData.length/ rowPerPage);
+    console.log(sumPage);
+    for (let index = 0; index < sumPage; index++) {
+        let currentBtn = document.createElement('div');
+        currentBtn.className = "pagi-item";
+        if(index == 0){
+            currentBtn.className += " active-pagi-item";
+        }
+        currentBtn.innerHTML = index + 1;
+        sourcePagi.appendChild(currentBtn);
+    }
+    let numberPagi = document.getElementsByClassName('pagi-item'); 
+    for (let index = 0; index < numberPagi.length; index++) {
+        numberPagi[index].addEventListener('click', function() { ShowCurrentPage(index + 1)});
+    }
+    
+    let nextBtn = document.createElement("div");
+    nextBtn.className = "pagi-item-next";
+    nextBtn.innerHTML = "Next";
+    nextBtn.addEventListener('click', function(){
+        HandlerNext();
+    })
+    sourcePagi.appendChild(nextBtn);
+}
+
+function HanlerPagiSlider(arr, item_per_page, current_page){
+    return arr.slice((current_page - 1)*item_per_page, current_page * item_per_page);
+}
+
+function HandlerBarFuc(){
+    let source = document.getElementById('small-template').innerHTML;
+    let template = Handlebars.compile(source);
+    let render = template({big_data: HanlerPagiSlider(bigData, rowPerPage, currentIndexPage)});
+    $("#container-table").html(render);
+    
+}
+
 async function InitCall(){
     await FetchData();
     CreateDot(dataList.length);
     CreateThumnails(dataList.length);
-    ShowSlider(sliderIndex);  
+    ShowSlider(sliderIndex);
+    HandlerBarFuc();
+    CreatePagination();  
 }
 
 InitCall();
